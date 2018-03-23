6489bacaa7d5b7bad2c37a093419ea5ecab0991f
oleg79 <0112oleg@gmail.com>
2018-03-24 01:09:29 +0300

    ADDED: react motion, basic animation

--- src/components/Playground/components/Cell/index.js
+++ src/components/Playground/components/Cell/index.js
@@ -1,14 +1,26 @@
+import { Motion, spring } from 'react-motion'
 import EmptyCell from '../EmptyCell'
 
 const XCell = () =>
-  <div className='cell'>
-    <div className='cell__value cell__value--x'>X</div>
-  </div>
+    <Motion defaultStyle={{x: 0}} style={{x: spring(10000)}}>
+      {
+        ({ x }) =>
+          <div className='cell' style={{ opacity: x / 10000 }}>
+            <div className='cell__value cell__value--x'>X</div>
+          </div>
+      }
+    </Motion>
+
 
 const OCell = () =>
-  <div className='cell'>
-    <div className='cell__value cell__value--o'>O</div>
-  </div>
+  <Motion defaultStyle={{x: 0}} style={{x: spring(10000)}}>
+    {
+      ({ x }) =>
+        <div className='cell' style={{ opacity: x / 10000 }}>
+          <div className='cell__value cell__value--o'>O</div>
+        </div>
+    }
+  </Motion>
 
 
 const Cell = ({ row, cell, cellType = null }) => ({
