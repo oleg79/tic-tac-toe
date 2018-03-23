ede7032107829183c05c3eef4e825fb5b796e4ae
oleg79 <0112oleg@gmail.com>
2018-03-24 00:04:52 +0300

    ADDED: basic server configs; EDITED: configs

--- client/index.php
+++ client/index.php
@@ -3,7 +3,7 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../../vendor/autoload.php';
 
 $app = new Silex\Application();
 
